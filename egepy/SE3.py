61845c4c69a0d060fbfaeb7462fe80b7e3eaa849
olaals <ola.alstad@ntnu.no>
2022-04-07 00:52:08 +0200

    Add partial project structure

---
+++ egepy/SE3.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+class SE3():
+    def __init__():
+        pass
+
+    def __wedge():
+        pass
+
+    def __vee():
+        pass
+
+    def __exp():
+        pass
+
+    def __log():
+        pass
+
+    def adjoint():
+        pass
+
+    def left_jacob():
+        pass
+
+    @classmethod
+    def from_vec(cls):
+        pass
+
+    def as_vec():
+        pass
+
+    @classmethod
+    def from_SE3_np():
+        pass
+
+    def as_SE3_np():
+        pass
+
+    @classmethod
+    def from_quat(cls):
+        pass
+
+    def as_quat():
+        pass
+
+    @classmethod
+    def from_euler(cls):
+        pass
+
+    def as_euler():
+        pass
+
+    @classmethod
+    def from_SO3(cls):
+        pass
+
+    def as_SO3():
+        pass
+
+    @classmethod
+    def from_SO3_np(cls):
+        pass
+
+    def as_SO3_np():
+        pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
---
+++ egepy/__init__.py
@@ -0,0 +1,4 @@
+from .SE3 import *
+from .SE2 import *
+from .SO2 import *
+from .SE3 import *
