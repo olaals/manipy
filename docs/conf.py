3ee7e9dbec4026e6cb5dee2f880a240d4f7e0bbd
Ola Alstad <olaals@macalo.local>
2022-04-14 18:45:42 +0200

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,3 @@
-import sphinx_rtd_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -19,8 +18,8 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'manipy'
-copyright = '2022, Ola Alstad'
-author = 'Ola Alstad'
+copyright = '2022, olaals'
+author = 'olaals'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
@@ -32,7 +31,6 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. manipy documentation master file, created by
-   sphinx-quickstart on Thu Apr 14 17:10:39 2022.
+   sphinx-quickstart on Thu Apr 14 18:40:13 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
